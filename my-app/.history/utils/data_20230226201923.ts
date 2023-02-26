4689ba3def2781642bd08e41347af4b39db024d7
TLRKiliann <philogenie@protonmail.com>
2023-02-26 20:26:49 +0100

    config has changed

---
+++ my-app/.history/utils/data_20230226201923.ts
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      slug: "free-shirt",
+      category: "Shirts",
+      image: "/images/shirt1.png",
+      price: 70,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Fit Shirt",
+      slug: "fit-shirt",
+      category: "Shirts",
+      image: "/images/shirt2.png",
+      price: 80,
+      brand: "Adidas",
+      rating: 3.2,
+      numReviews: 12,
+      countInStock: 30,
+      description: "A popular shirt",
+    },
+    {
+      name: "Golf Pant",
+      slug: "golf-Pant",
+      category: "Pants",
+      image: "/images/pant1.jpg",
+      price: 130,
+      brand: "Nike",
+      rating: 4.4,
+      numReviews: 33,
+      countInStock: 38,
+      description: "A popular pant",
+    },
+    {
+      name: "Fit Pant",
+      slug: "fit-Pant",
+      category: "Pants",
+      image: "/images/pant2.jpg",
+      price: 140,
+      brand: "Adidas",
+      rating: 4.2,
+      numReviews: 22,
+      countInStock: 42,
+      description: "A popular pant",
+    }
+  ]
+}
+export default data;
