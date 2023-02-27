4a6bc713a30f63864113b14082378e9904a8963b
TLRKiliann <philogenie@protonmail.com>
2023-02-27 11:05:50 +0100

    slug file created to catch product by name

--- my-app/pages/product/[slug].tsx
+++ my-app/pages/product/[slug].tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { useRouter } from 'next/router'
 import Layout from '../../components/Layout'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image'
 import data from '../../utils/data'
 
 export default function ProductScreen() {
@@ -16,7 +18,56 @@ export default function ProductScreen() {
 
   return (
     <Layout title={product.name}>
-      <h1>{product.name}</h1>
+      <div className="py-2">
+        <Link href={'/'}>Back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          >
+          </Image>
+        </div>
+        <div>
+          
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>{product.rating} of {product.numReviews} reviews</li>
+            <li>Description: {product.description}</li>
+          </ul>
+
+        </div>
+        <div>
+          <div className="card p-5">
+
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>${product.price}</div>  
+            </div>
+
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>  
+            </div>
+
+            <button
+              type='button'
+              className="primary-button w-full"
+            >
+              Add to cart
+            </button>
+
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
