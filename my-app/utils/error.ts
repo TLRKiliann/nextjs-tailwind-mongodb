d5c4a7eab79e63adb4c21cdaea8f3d7ca65473bb
TLRKiliann <philogenie@protonmail.com>
2023-04-13 17:18:21 +0200

    type added

--- my-app/utils/error.ts
+++ my-app/utils/error.ts
@@ -1,5 +1,15 @@
-const getError = (err) => 
-	err.response && err.response.data && err.response.data.message
-		? err.response.data.message
-		: err.message
+type ErrorResponse = {
+  response: {
+    data: {
+      message: string;
+    }
+  };
+  message: string;
+}
+
+const getError = (err: ErrorResponse): string => 
+  err.response && err.response.data && err.response.data.message
+    ? err.response.data.message
+    : err.message;
+
 export { getError };
